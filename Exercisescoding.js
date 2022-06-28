3123b1e373b43dc748a8fb48520602ebaeddb3e3
Alicia Barraza <Barraza.alicia@yahoo.com>
2022-06-27 23:26:18 -0500

    First Commit

---
+++ Exercisescoding.js
@@ -0,0 +1,21 @@
+function makeANumber() {
+     var myNum ="3";
+    console.log("myNum");
+}
+
+function makeAnInteger() {
+    var myInt ="4";
+    console.log("myInt");
+}
+    
+
+function makeAFloat() {
+    var myFloat ="34";
+    console.log("myFloat"); 
+}
+ 
+function makeZero() {
+    var zilch ="0";
+    console.log("zilch");
+}
+ 
